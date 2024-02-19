d21612c036774f3dce91bb27b4f3dce3375fdb02
aguayo99-beep <73196796+aguayo99-beep@users.noreply.github.com>
2024-02-19 22:16:23 +0100

    Update GymClassRouter.js

--- backend_fitic/routes/GymClassRouter.js
+++ backend_fitic/routes/GymClassRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 console.log("Hola1");
-const gymClassController = require('../backend_fitic/controllers/GymClassController');
+const gymClassController = require('../backend_fitic/controllers/gymClassController');
 
 //Obtener todas las clases de gimnasia
 router.get('/gymClasses', gymClassController.getAllGymClasses);
